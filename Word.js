712ed8849db4910e60805ff774ff91481ba473c3
Sheldon Perkins <csperkins18@gmail.com>
2019-11-20 01:20:06 -0500

    Word.js: letter object. TO DO: string representing word. for loop calling letter.js for display of the string

--- Word.js
+++ Word.js
@@ -1,3 +1,9 @@
 var letter = require("./Letter.js");
-Word =
+var Word = function (newWord){
 // array of `new` Letter objects representing the letters of the underlying word
+this.wArray= [];
+// TO DO:
+// string representing word. for loop calling letter.js for display of the string
+
+}
+
