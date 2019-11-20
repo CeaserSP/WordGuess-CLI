c983db2f60e13eb685f76cac203e345bdc2a1114
Sheldon Perkins <csperkins18@gmail.com>
2019-11-19 23:47:03 -0500

    constructor function for a letter, store letter

---
+++ Index.js
@@ -0,0 +1,8 @@
+// constructor function for a letter
+function Letter(letter) {
+    // store letter
+    this.character = letter;
+};
+
+// exporting our Student constructor
+module.exports = Student;
