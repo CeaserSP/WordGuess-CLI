6a33be0eda8f712b1e99d14f3c45d83abf6c42c1
Sheldon Perkins <csperkins18@gmail.com>
2019-11-20 01:08:23 -0500

    Letter.js: var letter so the function can be exported

--- Letter.js
+++ Letter.js
@@ -1,5 +1,5 @@
 // constructor function for a letter
-function Letter(letter) {
+var Letter= function(letter) {
     // store letter
     this.character = letter;
     // checks if the letter has been guessed default false
--- Word.js
+++ Word.js
@@ -1,2 +1,3 @@
 var letter = require("./Letter.js");
-
+Word =
+// array of `new` Letter objects representing the letters of the underlying word
