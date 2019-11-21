4a16e2b821764069f1dd3cfe43c203c58e8f2ed7
Sheldon Perkins <csperkins18@gmail.com>
2019-11-21 16:20:47 -0500

    call letter function store as object. Push object to wArray

--- Word.js
+++ Word.js
@@ -5,12 +5,15 @@ var Word = function (newWord) {
     // array of `new` Letter objects representing the letters of the underlying word
     this.wArray = [];
     // This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
-    var wordArray = function(){
-        for (var i = 0; i<lArray.lenth; i++){
+    var wordArray = function () {
+        for (var i = 0; i < lArray.lenth; i++) {
+            // call letter function
+            var wordObject = new letter(this.lArray[i]);
+            // push wordObject into wArray
+            this.wArray.push(wordObject);
+        };
+    };
 
-        }
-    }
-  
 
 
 
