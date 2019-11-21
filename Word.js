ad7ea0672e44769b2f03d03235b53f550bd79af0
Sheldon Perkins <csperkins18@gmail.com>
2019-11-21 15:34:45 -0500

    Use .split() on newWord to create an array of letters

--- Word.js
+++ Word.js
@@ -1,9 +1,11 @@
 var letter = require("./Letter.js");
-var Word = function (newWord){
-// array of `new` Letter objects representing the letters of the underlying word
-this.wArray= [];
-// TO DO:
-// string representing word. for loop calling letter.js for display of the string
+var Word = function (newWord) {
+    // Use .split() on newWord to create an array of letters
+    this.lArray = newWord.split('');
+    // array of `new` Letter objects representing the letters of the underlying word
+    this.wArray = [];
+    // TO DO:
+    // string representing word. for loop calling letter.js for display of the string
 
 }
 
