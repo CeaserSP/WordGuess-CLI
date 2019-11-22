a3385e288336116319882dc228c4b36575e38b09
Sheldon Perkins <csperkins18@gmail.com>
2019-11-22 18:42:00 -0500

    TO DO:...

--- Index.js
+++ Index.js
@@ -1,3 +1,4 @@
 var Word = require("./Word.js");
 var inquirer = require("inquirer")
 
+// TO DO: SET Randomly selects a word and uses the `Word` constructor to store it. * Prompts the user for each guess and keeps track of the user's remaining guesses
