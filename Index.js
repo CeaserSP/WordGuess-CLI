ecbd0a0198a89d4ff3e582c19b163a3bf80ed7c8
Sheldon Perkins <csperkins18@gmail.com>
2019-11-23 17:33:14 -0500

    call word functions and display initial string

--- Index.js
+++ Index.js
@@ -9,18 +9,21 @@ function Game() {
         if (err) {
             throw err;
         }
-            // get random word
-    // split word.txt on line
-    splitWords = data.split('/n')
+        // get random word
+        // split word.txt on line
+        splitWords = data.split('/n')
+        // get random line number
+        randomN = Math.floor(Math.random() * splitWords.lenght) + 1;
+        // get word on line
+        randomL = splitWords[randomN]
+        // call word
+        var word = new Word(randomL);
+        // call word.js function wordArray
+        word.wordArray();
+        // call displayWord to build displayString
+        word.displayWord();
+        // display initial string
+        console.log(word.dsiplayString)
     };
-    // get random line number
-    randomN = Math.floor(Math.random() * splitWords.lenght) +1;
-    // get word on line
-    randomL = splitWords[randomN]
-    // call word
-    var word = new Word(randomL);
-    // call word.js function wordArray
-    word.wordArray();
-// display wordString
-word.displayWord();
+
 };
