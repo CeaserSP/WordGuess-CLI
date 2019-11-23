502825903d173c6fa66124cc2374c62a7fecaaa3
Sheldon Perkins <csperkins18@gmail.com>
2019-11-23 16:32:15 -0500

    read in word.txt file

--- Index.js
+++ Index.js
@@ -3,7 +3,11 @@ var inquirer = require("inquirer")
 var fs = require("fs");
 
 // TO DO: SET Randomly selects a word and uses the `Word` constructor to store it. * Prompts the user for each guess and keeps track of the user's remaining guesses
-function Game(){
+function Game() {
     // Read in words
-
-}
+    fs.readFile('./words.txt'), function read(err, data) {
+        if (err) {
+            throw err;
+        }
+    };
+};
