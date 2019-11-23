f4683994ecaf3f4473eb4bc3454da00ff87d3536
Sheldon Perkins <csperkins18@gmail.com>
2019-11-23 16:01:18 -0500

    Game functions require fs for read in of .txt file

--- Index.js
+++ Index.js
@@ -1,4 +1,9 @@
 var Word = require("./Word.js");
 var inquirer = require("inquirer")
+var fs = require("fs");
 
 // TO DO: SET Randomly selects a word and uses the `Word` constructor to store it. * Prompts the user for each guess and keeps track of the user's remaining guesses
+function Game(){
+    // Read in words
+
+}
